582826ce20b8559b16f941881bcd694f040cf368
saurabh <ssroyal>
2020-02-29 02:48:06 +0530

    add MyDoublyList class

---
+++ src/Definition/MyDoublyList.java
@@ -0,0 +1,26 @@
+package Definition;
+
+import Adt.DoublyListAdt;
+
+public class MyDoublyList<E> implements DoublyListAdt {
+
+    @Override
+    public boolean add(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int search() {
+        return 0;
+    }
+}
